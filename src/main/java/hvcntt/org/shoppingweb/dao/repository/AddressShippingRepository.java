143b94c824736e84a0906feb30698e2de126ae47
Toan <toannxm.itedu@gmail.com>
2017-04-11 14:28:52 +0700

    fix add product to cart

---
+++ src/main/java/hvcntt/org/shoppingweb/dao/repository/AddressShippingRepository.java
@@ -0,0 +1,1 @@
+package hvcntt.org.shoppingweb.dao.repository;import org.springframework.data.jpa.repository.JpaRepository;import hvcntt.org.shoppingweb.dao.entity.ShippingInfo;public interface AddressShippingRepository extends JpaRepository<ShippingInfo, String> {}
---
+++ src/main/java/hvcntt/org/shoppingweb/dao/repository/RoleRepository.java
@@ -0,0 +1,1 @@
+package hvcntt.org.shoppingweb.dao.repository;import org.springframework.data.jpa.repository.JpaRepository;import hvcntt.org.shoppingweb.dao.entity.Role;public interface RoleRepository extends JpaRepository<Role, String> {    Role findByName(String rolename);}
